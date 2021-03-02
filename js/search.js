42d925b58eef2e4f00d99d63288c060ff6f0a0ca
gobltpqls99 <gobltpqls99@naver.com>
2021-03-02 15:42:25 +0900

    ..

--- js/search.js
+++ js/search.js
@@ -8,30 +8,23 @@ new Vue({
 		products: [],
 		searchProducts: [],
 	},
-    created() {
-        //만들어지자마자 할거
-        axios.get('../json/products.json')
-        .then((r) => {
-            this.products = r.data;
-            this.searchProducts = this.products;
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    },
-    methods: {
-        onSearchRemove(e){
-            this.search ='';
-            this.searchProducts = this.products;
-        },
-        onSearch(e) {
-            this.searchProducts = _.filter(this.products, o => o.title.includes(this.search));
-        }
-    }
+	created() {
+		axios.get('../json/products.json')
+		.then((r) => {
+			this.products = r.data;
+			this.searchProducts = this.products;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	},
+	methods: {
+		onSearchRemove(e) {
+			this.search = '';
+			this.searchProducts = this.products;
+		},
+		onSearch(e) {
+			this.searchProducts = _.filter(this.products, o => o.title.includes(this.search));
+		}
+	}
 });
-
-
-
-    // beforeCreate() {
-    // 화면이 만들어지기 전에 할거
-    // },
