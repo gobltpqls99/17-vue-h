868f0e7643c8c8def0341a155259868624e1a4a9
gobltpqls99 <gobltpqls99@naver.com>
2021-03-03 12:58:20 +0900

    ..

--- js/03.product.js
+++ js/03.product.js
@@ -1,51 +1,120 @@
-/****************** App ******************/
+/**************** App ******************/
 class App extends React.Component {
-  state = {
-    query: ''
-  }
-  handelChange = value => {
-    this.setState({
-      query: value
-    })
-  }
-  render() {
-    return (
-      <div className="container">
-        <TitleBar query={this.state.query} className="my-4"/>
-        <Search onChange={this.handelChange} />
-      </div>
-    )
-  }
+	products = []
+	state = {
+		query: '',
+		resultProducts: []
+	}
+	commitChange = value => {
+		this.setState({
+			query: value,
+			resultProducts: this.products.filter(v => v.title.includes(value))
+		})
+	}
+	componentDidMount() {
+		axios.get('../json/products.json')
+		.then(r => {
+			this.setState({
+				resultProducts: r.data,
+			})
+			this.products = r.data
+		})
+		.catch(e => {
+			console.log(e)
+		})
+	}
+	render() {
+		let { query, resultProducts } = this.state 
+		return (
+			<div className="container">
+				<TitleBar query={query} className="my-4" />
+				<Search onChange={this.commitChange} />
+				<Products products={resultProducts} />
+			</div>
+		)
+	}
 }
 
-/****************** TitleBar ******************/
+/**************** TitleBar ******************/
 class TitleBar extends React.Component {
-  render() {
-    return (
-      <div className="jumbotron">
-        <h1>
-          React 쇼핑몰
-          <br />
-          <small>쇼핑몰 검색 | {this.props.query}</small>
-        </h1>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="jumbotron">
+				<h1>
+					React 쇼핑몰
+					<br />
+					<small>쇼핑몰 검색 | {this.props.query}</small>
+				</h1>
+			</div>
+		)
+	}
 }
 
 
-/****************** Search ******************/
+/**************** Search ******************/
 class Search extends React.Component {
-  handelChange = e => {
-    this.props.onChange(e.target.value)
-  }
-  render() {
-    return (
-      <form className="form-search">
-        <input className="form-control" type="text" onChange={this.handelChange} autoFocus />
-      </form>
-    )
-  }
+	state = {
+		query: ''
+	}
+	inputRef = React.createRef() // DOM을 품는 변수가 되어라
+	handleChange = e => {
+		this.onChange(e.target.value)
+	}
+	handleRemove = e => {
+		this.onChange('')
+		// ref는 current로 접근하여야 한다.
+		this.inputRef.current.focus()
+	}
+	onChange = value => {
+		this.props.onChange(value)
+		this.setState({
+			query: value
+		})
+	}
+	render() {
+		return (
+			<form className="form-search">
+				<input className="form-control py-2" type="text" onChange={this.handleChange} autoFocus value={this.state.query} ref={this.inputRef /* 나를 inputRef에 담아라 */} />
+				{
+					this.state.query.length > 0
+					? <i className="fa fa-times-circle bt-remove" onClick={this.handleRemove} />
+					: ''
+				}
+			</form>
+		)
+	}
 }
+
+/**************** Products ******************/
+class Products extends React.Component {
+	render() {
+		return (
+			<div className="list-wrapper">
+				{ this.props.products.map(v => <Product value={v} key={v.id+'_'+v.title}/>) }
+			</div>
+		)
+	}
+}
+
+/**************** Product ******************/
+class Product extends React.Component {
+	render() {
+		let { src, title, price } = this.props.value
+		return (
+			<div className="list">
+				<div className="img-wrap">
+					<img src={src} className="w-100"/>
+				</div>
+				<div className="title">{title}</div>
+				<div className="price">${price}</div>
+			</div>
+		)
+	}
+}
+
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('app'))
